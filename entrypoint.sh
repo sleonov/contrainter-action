969cc5e12503bba4ac2aabe3bbadb1351700959c
Sergey Leonovich <sergey.leonovich@transunion.com>
2024-06-26 19:45:35 -0500

    Update entrypoint to calculate value

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,20 @@
-#!/bin/sh -l
+#!/bin/bash
 
 echo "Parameter passed: $1"
-echo "value=TO_BE_DETERMINED" >> $GITHUB_OUTPUT
+value="UNKNOWN"
+
+case "$1" in
+  cpu)
+    value=$(cat /proc/cpuinfo |grep -c processor| awk '{print $1}')
+    ;;
+  memory)
+    value=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')
+    ;;
+  *)
+    echo "Invalid value passed, must be 'cpu' or 'memory'!"
+    ;;
+esac
+
+git_hub_output="value=$value"
+echo "Returning $git_hub_output"
+echo "$git_hub_output" >> $GITHUB_OUTPUT
