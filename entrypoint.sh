80c5baefdabd24f0c75738c998eae61ea6dc5f0e
Sergey Leonovich <sleonovich@gmail.com>
2024-06-26 20:04:53 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,12 @@
 echo "Parameter passed: $1"
 value="UNKNOWN"
 
-local_output_file = "./github.output"
-date >> $local_output_file
-
 case "$1" in
   cpu)
-    value=$(cat /proc/cpuinfo |grep -c processor| awk '{print $1}')
+    value=$(grep -c processor /proc/cpuinfo | awk '{print $1}')
     ;;
   memory)
-    value=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')
+    value=$(grep MemTotal /proc/meminfo | awk '{print $2}')
     ;;
   *)
     echo "Invalid value passed, must be 'cpu' or 'memory'!"
@@ -20,5 +17,4 @@ esac
 
 git_hub_output="value=$value"
 echo "Returning $git_hub_output"
-echo "git_hub_output" >> $local_output_file
 echo "$git_hub_output" >> $GITHUB_OUTPUT
