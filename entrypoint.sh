8212b917ad1c74719c980113a514e0b120388ec3
Sergey Leonovich <sleonovich@gmail.com>
2024-06-26 20:02:09 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 echo "Parameter passed: $1"
 value="UNKNOWN"
 
+local_output_file = "./github.output"
+date >> $local_output_file
+
 case "$1" in
   cpu)
     value=$(cat /proc/cpuinfo |grep -c processor| awk '{print $1}')
@@ -17,4 +20,5 @@ esac
 
 git_hub_output="value=$value"
 echo "Returning $git_hub_output"
+echo "git_hub_output" >> $local_output_file
 echo "$git_hub_output" >> $GITHUB_OUTPUT
